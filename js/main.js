35c76e0e105a9887ec49038300938ce868cb0aeb
sampath <you@example.com>
2020-07-14 16:11:04 +0530

    Commited

--- js/main.js
+++ js/main.js
@@ -10,10 +10,9 @@ fetch('data.json').then(response=>{
 	var child_two=document.querySelector(".child_two");
     
     var profile=(basic_info)=>{
-    	basic_info.map(i,index)=>{
 	// console.log(basic_info.name);
 	var name=document.createElement("h2");
-	name.textContent= i.basic_info.name;
+	name.textContent = basic_info.name;
 	child_one.append(name);
 
 	var profession=document.createElement("h4");
@@ -27,26 +26,28 @@ fetch('data.json').then(response=>{
 	var email=document.createElement("a");
 	email.href="mailto:"+basic_info.email;
 	email.textContent=basic_info.email;
+	email.classList.add("link");
 	child_one.append(email);
 
 	var br=document.createElement("br");
 	child_one.append(br);
 	
-	var br=document.createElement("br");
-	child_one.append(br);
-
+	
     var phone=document.createElement("a");
 	phone.href="tel:"+basic_info.phone;
 	phone.textContent=basic_info.phone;
+	phone.classList.add("link");
 	child_one.append(phone);
 
 	var adress=document.createElement("p");
 	adress.textContent=basic_info.adress;
 	child_one.append(adress);
  }
-}
-
-	
-	var details=(details_info)=>{
+   var details=(details_info)=>{
    //console.log(details_info);
-	}
+   var summaryheading=document.createElement("h2");
+   summaryheading.textContent="Summary";
+   var hr1=document.createElement("hr");
+   child_two.append(summaryheading);
+   child_two.append(hr1)
+}
